7ac4dc194671923108d0bace7f7ab72d974cb3d2
tiffa-cute <sulboymnyama@gmail.com>
2025-02-03 07:20:14 -0800

    Update settings.py

--- hotel_project/settings.py
+++ hotel_project/settings.py
@@ -94,21 +94,10 @@ WSGI_APPLICATION = 'hotel_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 
-# Password validation
+
+
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
